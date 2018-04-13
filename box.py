be2bcb6a1b41826d285a533f43411a4eea97c747
Goeste <felix@goeste.de>
2018-04-13 09:14:48 +0200

    Update box.py
    added: pnext/previous song, play, turn shuffle on/of, vol up/down/mute
    (Volume with help of G10DRAS at https://gist.github.com/G10DRAS/2eb9ec63821850b19af8a241735ff599)

--- box.py
+++ box.py
@@ -5,50 +5,74 @@ import re
 from CardList import CardList
 from Reader import Reader
 import sys
-
-
+ 
+ 
 def connectMPD():
-	try:
-		client = MPDClient()               # create client object
-		client.timeout = 200               # network timeout in seconds (floats allowed), default: None
-		client.idletimeout = None  
-		print "Connecting..."
-		client.connect("localhost", 6600) 
-		print "Connected!"
-		return client
-	except:
-		print 'Could not connect to MPD server'
-
+        try:
+                client = MPDClient()               # create client object
+                client.timeout = 200               # network timeout in seconds (floats allowed), default: None
+                client.idletimeout = None
+                print "Connecting..."
+                client.connect("localhost", 6600)
+                print "Connected!"
+                return client
+        except:
+                print 'Could not connect to MPD server'
+ 
 def play(client, plist):
-	try:
-		client.stop()
-		client.clear()
-		client.add(plist)
-		if re.search('playlist',plist):
-			client.shuffle()
-		client.play()
-	except:
-		print 'Could not play playlist %s' % plist 
-
+        try:
+                client.stop()
+                client.clear()
+                client.add(plist)
+#               if re.search('playlist',plist):     #un-comment for turning on shuffle at start directly
+#                       client.shuffle()            #un-comment this line as well for direct-shuffle
+                client.play()
+        except:
+                print 'Could not play playlist %s' % plist
+ 
 reader = Reader()
 cardList = CardList()
-
+ 
 print 'Ready: place a card on top of the reader'
-
+ 
 while True:
-	try:
-		card = reader.readCard()
-		print 'Read card', card
-		plist = cardList.getPlaylist(card)
-		print 'Playlist', plist
-		if plist != '':
-			client = connectMPD()
-			if plist=='pause':
-				client.pause()
-			else:
-				play(client, plist)
-			client.close()
-	except KeyboardInterrupt:
-		sys.exit(0)
-	except:
-		pass
+        try:
+                card = reader.readCard()
+                print 'Read card', card
+                plist = cardList.getPlaylist(card)
+                print 'Playlist', plist
+                if plist != '':
+                        client = connectMPD()
+                        if plist=='pause':                  #pause
+                                client.pause()
+                        elif plist=='next':                 #next song
+                                client.next()
+                        elif plist=='play':                 #play song
+                                client.play()
+                        elif plist=='previous':             #previous song
+                                client.previous()
+                        elif plist=='shuffle':              #turn on/off shuffle
+                                client.shuffle()
+                        elif plist=='voldown':              #volume down in steps
+                                client.status()['volume']
+                                client.status()['state']
+                                level = int(client.status()['volume']) - 10 #change to desired amount for decreasing volume
+                                level = max(min(level, 100), 0)
+                                client.setvol(level)
+                                client.status()['volume']
+                        elif plist=='volup':                #volume up in steps
+                                client.status()['volume']
+                                client.status()['state']
+                                level = int(client.status()['volume']) + 10 #change to desired amount for increasing volume
+                                level = max(min(level, 100), 0)
+                                client.setvol(level)
+                                client.status()['volume']
+                        elif plist=='mute':                 #volume mute
+                                client.setvol(0)
+                        else:
+                                play(client, plist)
+                        client.close()
+        except KeyboardInterrupt:
+                sys.exit(0)
+        except:
+                pass
